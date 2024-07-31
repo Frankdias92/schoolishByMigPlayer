d2278f04480eb240edba13fdd3f4102bbf3a22c4
Miguel Tibincoski <tibincoskimiguel@gmail.com>
2024-07-30 23:01:55 -0300

    chore: Update HomeBlock and Navbar styles

---
+++ src/styles/colors.tsx
@@ -0,0 +1,12 @@
+export default {
+  mainColor: "rgb(150, 0, 200)",
+  mainColorLight: "rgb(100, 100, 200)",
+  secondaryColor: "rgb(200, 150, 250)",
+  textColor: "rgb(250, 250, 250)",
+
+  alerts: {
+    error: "rgb(255, 100, 100)",
+    warning: "rgb(255, 255, 100)",
+    success: "rgb(50, 128, 50)"
+  }
+};
