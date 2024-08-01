381c5aa55c73f07990fa1e1578097755390ebb7f
Miguel Tibincoski <tibincoskimiguel@gmail.com>
2024-07-31 21:52:56 -0300

    chore: Update Firebase configuration and add Firebase Emulator connection (ONLY IN LOCALHOST)

--- src/services/firebase.tsx
+++ src/services/firebase.tsx
@@ -1,31 +1,34 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, connectAuthEmulator } from 'firebase/auth';
-import 'firebase/analytics';
-import { getAnalytics } from 'firebase/analytics';
+import { initializeApp } from "firebase/app";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+import "firebase/analytics";
+import { getAnalytics } from "firebase/analytics";
 
 // Configurações do Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyCLNRhky8snqJ4QXB31Sa9sIvD3DiIkBwo",
-    authDomain: "schoolish-official.firebaseapp.com",
-    projectId: "schoolish-official",
-    storageBucket: "schoolish-official.appspot.com",
-    messagingSenderId: "510922615291",
-    appId: "1:510922615291:web:cf12894c2209f700b9435e",
-    measurementId: "G-N0JH48SBJX"
+  apiKey: "AIzaSyCLNRhky8snqJ4QXB31Sa9sIvD3DiIkBwo",
+  authDomain: "schoolish-official.firebaseapp.com",
+  projectId: "schoolish-official",
+  storageBucket: "schoolish-official.appspot.com",
+  messagingSenderId: "510922615291",
+  appId: "1:510922615291:web:cf12894c2209f700b9435e",
+  measurementId: "G-N0JH48SBJX",
 };
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const hooks = {
-    getUserData: () => auth.currentUser || null
+  getUserData: () => auth.currentUser || null,
+};
+
+// Emulador do Firebase
+const hostname = window.location.hostname;
+const localIPs = ["localhost", "127.0.0.1", "::1"];
+
+const localIPRegex =
+  /^(?:192\.168|10|172\.(?:1[6-9]|2\d|3[01]))\.\d{1,3}\.\d{1,3}$/;
+if (localIPs.includes(hostname) || localIPRegex.test(hostname)) {
+  connectAuthEmulator(auth, "http://127.0.0.1:9099");
 }
 
-connectAuthEmulator(auth, "http://127.0.0.1:9099");
-
-export { 
-    app, 
-    auth,
-    analytics,
-    hooks
-};
+export { app, auth, analytics, hooks };
