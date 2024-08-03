646e679eefa1618adb83fbfbb9468097ea53f9e5
Miguel Tibincoski <tibincoskimiguel@gmail.com>
2024-08-03 10:09:07 -0300

    feat: Refactor React Router configuration to use new Routes and Route components

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import "react";
 import * as ReactDOM from "react-dom/client";
-import { BrowserRouter, createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import Home from "./pages/Home";
@@ -8,26 +8,7 @@ import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 
-import './styles/firebaseEmulatorWarning.css';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/signup",
-    element: <Signup />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/dashboard",
-    element: <Dashboard />
-  }
-]);
+import "./styles/firebaseEmulatorWarning.css";
 
 ReactDOM.createRoot(
   document.getElementById("root") ?? document.createElement("div")
@@ -35,6 +16,11 @@ ReactDOM.createRoot(
   <BrowserRouter>
     <SpeedInsights />
 
-    <RouterProvider router={router} />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
   </BrowserRouter>
 );
